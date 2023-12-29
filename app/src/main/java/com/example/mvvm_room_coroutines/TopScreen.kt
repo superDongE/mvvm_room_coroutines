51503e202a5c6988b2836d985b39d1372b5abb82
haedong.lim <haedong.lim@olive-hc.com>
2023-12-29 13:33:20 +0900

    Top Screen 구성 완료

--- app/src/main/java/com/example/mvvm_room_coroutines/TopScreen.kt
+++ app/src/main/java/com/example/mvvm_room_coroutines/TopScreen.kt
@@ -4,17 +4,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 @Composable
 fun TopScreen(list: List<Conversion>) {
     val selectedConversion: MutableState<Conversion?> = remember { mutableStateOf(null) }
     val inputText: MutableState<String> = remember { mutableStateOf("") }
+    val typedValue = remember { mutableStateOf("0.0") }
 
     ConversionMenu(list = list) {
         selectedConversion.value = it
     }
 
     selectedConversion.value?.let {
-        InputBlock(conversion = it, inputText = inputText)
+        InputBlock(conversion = it, inputText = inputText) { input ->
+            typedValue.value = input
+        }
+    }
+
+    if (typedValue.value != "0.0") {
+        val input = typedValue.value.toDouble()
+        val multiply = selectedConversion.value!!.multiplyBy
+        val result = input * multiply
+
+        // rounding off the result 4 decimal places
+        val df = DecimalFormat("#.####")
+        df.roundingMode = RoundingMode.DOWN
+        val roundedResult = df.format(result)
+
+        val message1 = "${typedValue.value} ${selectedConversion.value!!.convertFrom} is equal to"
+        val message2 = "$roundedResult ${selectedConversion.value!!.convertTo}"
+
+        ResultBlock(message1 = message1, message2 = message2)
     }
 }
--- app/src/main/java/com/example/mvvm_room_coroutines/inputBlock.kt
+++ app/src/main/java/com/example/mvvm_room_coroutines/inputBlock.kt
@@ -1,19 +1,26 @@
 package com.example.mvvm_room_coroutines
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -24,6 +31,8 @@ fun InputBlock(
     conversion: Conversion,
     inputText: MutableState<String>,
     modifier: Modifier = Modifier,
+    context: Context = LocalContext.current,
+    calculate: (String) -> Unit,
 ) {
     Column(
         modifier = modifier.padding(0.dp, 20.dp, 0.dp, 0.dp)
@@ -51,6 +60,27 @@ fun InputBlock(
                 modifier = modifier
                     .padding(10.dp, 30.dp, 0.dp, 0.dp)
                     .fillMaxWidth(0.35F)
+            )
+        }
+
+        Spacer(modifier = modifier.height(20.dp))
+
+        OutlinedButton(
+            onClick = {
+                if (inputText.value != "") {
+                    calculate(inputText.value)
+                } else {
+                    Toast.makeText(context, "Please, enter your value", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            },
+            modifier = modifier.fillMaxWidth(1F)
+        ) {
+            Text(
+                text = "Convert",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
             )
         }
     }
